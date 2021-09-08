052b452c400ccb6cdbd14b54a5fb15edfda6c8fd
Mo Zhou <cdluminate@gmail.com>
2021-09-08 10:22:33 -0400

    models/*amd: update warning message

--- robrank/models/rc2f2ramd.py
+++ robrank/models/rc2f2ramd.py
@@ -21,5 +21,6 @@ class Model(rc2f2.Model):
     is_advtrain_ramd = True
 
     def post_init_hook(self):
-        utils.warn('reducing pgd step to 7 or it will diverge')
+        utils.warn('reducing advtrain_pgditer to 7 or it will collapse')
+        utils.warn('this warning is irrelevant to attack')
         self.config.advtrain_pgditer = 7
