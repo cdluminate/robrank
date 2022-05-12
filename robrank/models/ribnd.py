aa317bde5d028a655bae49e4f6f6381779ec71ba
Mo Zhou <cdluminate@gmail.com>
2022-05-12 18:42:41 -0400

    misc: bump year in license header

--- robrank/models/ribnd.py
+++ robrank/models/ribnd.py
@@ -1,5 +1,5 @@
 '''
-Copyright (C) 2019-2021, Mo Zhou <cdluminate@gmail.com>
+Copyright (C) 2019-2022, Mo Zhou <cdluminate@gmail.com>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
--- robrank/models/template_rank.py
+++ robrank/models/template_rank.py
@@ -1,5 +1,5 @@
 '''
-Copyright (C) 2019-2021, Mo Zhou <cdluminate@gmail.com>
+Copyright (C) 2019-2022, Mo Zhou <cdluminate@gmail.com>
 
 
 Licensed under the Apache License, Version 2.0 (the "License");
