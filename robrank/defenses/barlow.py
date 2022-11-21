272f99b1057065ae01c799a49288743a25ac76db
Mo Zhou <cdluminate@gmail.com>
2022-11-21 12:21:56 -0500

    impl: barlow twins [wip]

--- robrank/defenses/barlow.py
+++ robrank/defenses/barlow.py
@@ -20,10 +20,13 @@ https://arxiv.org/pdf/2103.03230.pdf
 Let's see if this helps reducing collapse issue a little bit
 '''
 
+from typing import List
 import torch as th
 import torch.nn.functional as F
+from collections import Counter
 import rich
 console = rich.get_console()
+__lambda__ = 5e-3
 
 
 def _off_diagonal(matrix: th.Tensor):
@@ -58,7 +61,7 @@ def test__diagonal():
     assert abs(res - ref) < 1e-5, 'implementation bug'
 
 
-def _barlow_twins(z_a: th.Tensor, z_b: th.Tensor, *, lam: float = 5e-3):
+def _barlow_twins(z_a: th.Tensor, z_b: th.Tensor, *, lam: float = __lambda__):
     '''
     computation kernel... the most core operations
     we follow the pseudo-code of https://arxiv.org/pdf/2103.03230.pdf
@@ -91,3 +94,71 @@ def test__barlow_twins():
     b = th.rand(10, 10)
     bt = _barlow_twins(a, b)
     assert not th.isnan(bt), 'implementation bug'
+
+
+def assemble_same_class(tensors: List[th.Tensor], labels: List[th.Tensor], *,
+                        thres: int = 3) -> List[th.Tensor]:
+    '''
+    assemble vectors of the same class together
+    all the given labels should be the same, i.e., labels[0] == labels[1]
+
+    input:
+      tensors: list of tensors
+        [z_a_1, z_b_1, z_a_2, z_b_2]
+      labels:
+        labels corresponding to the above tensors
+    output:
+      asm:
+        [[z_a_cls1, z_b_cls1], [z_a_cls2, z_b_cls2], ...]
+      we select the classes that appeared at least `thres` times on a single
+      side for A/B.
+    '''
+    counter = Counter()
+    for i in range(0, len(labels), 2):
+        counter.update(labels[i].tolist())
+    selections = [k for (k, v) in counter.items() if v >= thres]
+    #print('asm: selections', selections)
+    asm = []
+    for cls in selections:
+        acls = [x[l == cls] for (x, l) in zip(tensors[0::2], labels[0::2])]
+        #print('acls', cls, 'sizes', [x.shape for x in acls])
+        acls = th.vstack(acls)
+        bcls = [x[l == cls] for (x, l) in zip(tensors[1::2], labels[1::2])]
+        #print('bcls', cls, 'sizes', [x.shape for x in bcls])
+        bcls = th.vstack(bcls)
+        asm.append([acls, bcls])
+    return asm
+
+
+def test_assemble_same_class():
+    '''
+    note, use the following command to enable stdout printing
+    $ pytest -v -x -k barlow -s
+    '''
+    xa = th.rand(100, 10)
+    xb = th.rand(100, 10)
+    la = th.randint(0, 10, (100,))
+    lb = la.clone()
+    thres = 3
+    asm = assemble_same_class([xa, xb], [la, lb], thres=thres)
+    for (xa, xb) in asm:
+        assert xa.dim() == 2
+        assert xa.size(0) >= 3
+        assert xb.dim() == 2
+        assert xb.size(0) >= 3
+        assert xa.shape == xb.shape
+        print('xa.shape', xa.shape, 'xb.shape', xb.shape)
+
+
+def barlow_twins(z_a: th.Tensor, l_a: th.Tensor,
+                 z_b: th.Tensor, l_b: th.Tensor,
+                 *, lam: float = __lambda__):
+    '''
+    wrapped version of barlow twins
+    input:
+    z_a: a batch of arbitrary class
+    l_a: labels of z_a
+    z_b: another batch of arbitrary class
+    note, class(z_a) should be aligned with class(z_b)
+    '''
+    pass
