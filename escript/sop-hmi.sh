3d85fa82f6a3fefb2a32d649a1549b5278707cf3
Mo Zhou <cdluminate@gmail.com>
2023-03-21 12:12:07 -0400

    escript: update sop-hm and sop-hmi

--- escript/sop-hmi.sh
+++ escript/sop-hmi.sh
@@ -1,4 +1,17 @@
-export CUDA_VISIBLE_DEVICES=0,1
+#!/bin/sh
+set -e
+config="sop:rres18ghmetsmi:ptripletN"
+ckpt="logs_sop-rres18ghmetsmi-ptripletN/lightning_logs/version_0/checkpoints/epoch=74-step=39900.ckpt"
+
+# sometimes the communication through NCCL will make training hang.
 export NCCL_P2P_DISABLE=1
-python3 bin/train.py -C sop:rres18ghmetsmi:ptripletN
-python3 bin/swipe.py -p rob224 -C logs_sop-rres18ghmetsmi-ptripletN/lightning_logs/version_0/checkpoints/epoch=74-step=39900.ckpt
+
+# training in distributed data parallel
+export CUDA_VISIBLE_DEVICES=0,1
+python3 bin/train.py -C ${config}
+
+# evaluation (only support single GPU mode)
+export CUDA_VISIBLE_DEVICES=0
+python3 bin/swipe.py -p rob224 -C ${ckpt}
+
+# TODO
